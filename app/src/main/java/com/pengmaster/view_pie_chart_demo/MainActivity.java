edda7a5e7b90bb586de718f37ed8d5d435c361b4
pengMaster <946549990@qq.com>
2020-05-01 16:21:03 +0800

    first-commit

---
+++ app/src/main/java/com/pengmaster/view_pie_chart_demo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pengmaster.view_pie_chart_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
